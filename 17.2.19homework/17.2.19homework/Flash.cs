b3831f03ea17c2c3b869e53549798025cb23b17b
sagitush <44786027+sagitush@users.noreply.github.com>
2019-02-19 21:36:43 +0200

    Add files via upload

---
+++ 17.2.19homework/17.2.19homework/Flash.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._2._19homework
+{
+    class Flash : IFlash, Human
+    {
+        public  int Voltage { get;private set; }
+
+        public Flash(int voltage, string name,int age):base(name,age)
+        {
+            Voltage = voltage;
+            Name = name;
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return Name;
+            }
+
+            set
+            {
+                if (Name!=null)
+                {
+                    this.Name = Name;
+                }
+            }
+        }
+
+        public void FireLightnings()
+        {
+            Console.WriteLine("fire,fire");
+        }
+        public void ActiveSuperPowers()
+        {
+            FireLightnings();
+        }
+    }
+}
